887079c8518a5b4bf6a8a06f9ab4e7f980d8503e
Gabriel Domingues Pasquantonio <pasquantonioo@gmail.com>
2020-05-03 19:54:21 -0300

    added footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
 import NavBar from './components/NavBar/NavBar.jsx'
-
+import Footer from './components/Footer/Footer.jsx'
 function App() {
   return (
-  <NavBar />   
+  <> 
+  <NavBar />
+  <Footer />  
+  </>
   );
 }
 
