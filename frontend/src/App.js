b634f714462f2f8390bf586adc5638e7515b8b04
gabrielpasquantonio <64622616+gabrielpasquantonio@users.noreply.github.com>
2020-05-25 18:11:03 -0300

    added component 5

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,11 +3,13 @@ import './App.css';
 import Header from './components/NavBar/NavBar.jsx'
 import Footer from './components/Footer/Footer.jsx'
 import ThumbNail from './components/Thumbnails/Thumbnail';
+import Descuento from './components/Descuento/Descuento';
 function App() {
   return (
   <> 
-  <Header />
+  <Header/>
   <ThumbNail/>
+  <Descuento/>
   <Footer />
     
   </>
